a0528c24c915d4562c3def03739eff4892e69981
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-11-16 23:38:17 +0300

    finished setting up the user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
- 
+    getName(){
+        return "My name is Yonatan Tizazu"
+      };
+    
+      getHobby(){
+        return {
+          hobby: "I love coding and playing football, and I also enjoy watching action movies and reading books."
+        };
+      };
+    
+      getDream(){
+        return {
+          hobby: `My dream is to harness my skills in software engineering and hardware to create innovative solutions that address real-world challenges, particularly in Africa. I am passionate about blending backend development, machine learning, and cutting-edge technologies like blockchain to build scalable, impactful systems.`
+        }
+      }
  
 }
